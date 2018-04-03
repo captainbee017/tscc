77480aedba978fdab8bea0d1891be096251990e7
Abhishek <abhishek.jung@gmail.com>
2018-04-03 20:36:21 +0545

    sample model design

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 QUERY_TYPE = (
@@ -13,11 +13,27 @@ STATUS_CHOICE = (
     ('Completed', 'Completed'),
 )
 
+USER_ROLES = (
+    ('Typist', 'Typist'),
+    ('Editor', 'Editor'),
+    ('Superuser','Superuser'),
+)
+
 
 class CallDetail(models.Model):
     query_type = models.CharField(max_length=20, choices=QUERY_TYPE, default='Query')
     call_time = models.CharField(max_length=20, null=True, blank=True)
     status = models.CharField(max_length=30, choices=STATUS_CHOICE, default='Pending')
+    author = models.ForeignKey(User, related_name='call_author')
 
     def __str__(self):
         return self.query_type
+
+
+class User(AbstractUser):
+    """
+    Typist; can only write/create forms(both query and complains
+    Editor; can both read/write forms
+    Superuser: All permissions
+    """
+    user_role = models.CharField(max_length=20, choices=USER_ROLES, default='Editor')

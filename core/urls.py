80bd5386f63d0102473ed67ff3faee80301f8c28
Abhishek <abhishek.jung@gmail.com>
2018-04-13 11:39:50 +0545

    manage ticket fix

--- core/urls.py
+++ core/urls.py
@@ -20,11 +20,13 @@ urlpatterns = [
     path('manage-calls/list/', sv.ManageCallDetails.as_view(), name='manage_call_details'),
 
     path('new-ticket/', sv.NewTicket.as_view(), name='new_ticket'),
-    path('reports/', sv.Report.as_view(), name='report'),
+    path('report/', sv.Report.as_view(), name='report'),
     path('login/', LoginView.as_view(), name='login_page'),
 
     path('new-user/', UserCreateView.as_view(), name='add_new_user'),
     path('change-password/<username>/', UserChangePasswordView.as_view(), name='change_password'),
     path('deactivate/<username>/', DeactivateCsrView.as_view(), name='deactivate_csr'),
+
+    path('manage-tickets/<ticket_type>/', sv.ManageTickets.as_view(), name='manage_tickets'),
 ]
 
--- core/views/supervisor.py
+++ core/views/supervisor.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
+from django.views.generic import ListView
 from django.views.generic import TemplateView
+
+from core.mixins import SuperuserOnlyMixin
+from core.models import CallDetail, QUERY_TYPE
 
 
 class CategorySettings(TemplateView):
@@ -17,4 +21,23 @@ class NewTicket(TemplateView):
 
 class Report(TemplateView):
     template_name = "core/ticket_report.html"
+
+
+class ManageTickets(SuperuserOnlyMixin, ListView):
+    """
+    Manage tickets:
+    receive ticket categories from kwargs and filter categories as such
+    """
+    model = CallDetail
+    template_name = 'superuser/manage_ticket.html'
+
+    def get_queryset(self, **kwargs):
+        ticket_type = self.kwargs['ticket_type']
+        qt = [a[0] for a in QUERY_TYPE if a[1].lower() == ticket_type]
+        return super().get_queryset(**kwargs).filter(category__call_type=qt[0])
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        ctx['ticket_type'] = self.kwargs['ticket_type']
+        return ctx
 
